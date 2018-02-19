d2967b4a8a596eeac061b083c1bb57a6fbc958fb
Annie Thwing <athwing@yahoo.com>
2018-02-19 13:41:23 -0700

    Checked and Fixed linkRare function

--- R/linkErrorCheck.R
+++ R/linkErrorCheck.R
@@ -1,37 +1,37 @@
 linkErrorCheck <-
-function(x,y,npl,z=NULL){
-  
-  # Check that x is a matrix
-  if(is.null(nrow(x)) | is.null(ncol(x))){stop("Error: x must be a matrix.")}
-  
-  # Check y is a vector
-  if(!is.vector(y)){stop("Error: y must be a vector.")}
-
-  # Check npl is a vector
-  if(!is.vector(npl)){stop("Error: npl must be a vector.")}
-  
-  # Check that length(npl)==length(y), if it doesn't send an error
-  if(length(npl)!=length(y)){stop("Error: the length of npl must be equal to the length of y.")}
-  
-  # Check that nrow(x)==length(y), if it doesnt send an error
-  if(nrow(x)!=length(y)){stop("Error: The length of y must equal the number of rows in x.")}
-  
-  # Check y==0 or y==1, if it doesn't send an error
-  # if((length(y[y==0&!is.na(y)])+length(y[y==1&!is.na(y)])+length(y[is.na(y)]))==length(y)){stop("Error: y must be 0 or 1.")}
-  temp <- y
-  temp <- na.omit(temp)
-  if((sum(temp==1)+sum(temp==0))!=length(temp)){stop("Error: y must be 0 or 1.")}
-  
-  # Z must be entered as a matrix if if there is only one covariate, check length(ncol(z))>0 (make sure it is a matrix)
-  # check nrow(z)==length(y)
-  # check z is a matrix, and that different values of z work
-  
-  if(!is.null(z)){
-    if(is.vector(z)){
-      if(length(z)!=nrow(x)){stop("Error: length(z) must be equal to nrow(x).")}
-    }else if(!is.null(nrow(z)) & !is.null(ncol(z))){
-      if(nrow(z)!=length(y)){stop("Error: nrow(z) must be equal to length(y).")}
-    }else{stop("Error: z must either be a vector or a matrix.")}
+  function(x,y,npl,z=NULL){
+    
+    # Check that x is a matrix
+    if(is.null(nrow(x)) | is.null(ncol(x))){stop("Error: x must be a matrix.")}
+    
+    # Check y is a vector
+    if(!is.vector(y)){stop("Error: y must be a vector.")}
+    
+    # Check npl is a vector
+    if(!is.vector(npl)){stop("Error: npl must be a vector.")}
+    
+    # Check that length(npl)==length(y), if it doesn't send an error
+    if(length(npl)!=length(y)){stop("Error: the length of npl must be equal to the length of y.")}
+    
+    # Check that nrow(x)==length(y), if it doesnt send an error
+    if(nrow(x)!=length(y)){stop("Error: The length of y must equal the number of rows in x.")}
+    
+    # Check y==0 or y==1, if it doesn't send an error
+    # if((length(y[y==0&!is.na(y)])+length(y[y==1&!is.na(y)])+length(y[is.na(y)]))==length(y)){stop("Error: y must be 0 or 1.")}
+    temp <- y
+    temp <- na.omit(temp)
+    if((sum(temp==1)+sum(temp==0))!=length(temp)){stop("Error: y must be 0 or 1.")}
+    
+    # Z must be entered as a matrix if if there is only one covariate, check length(ncol(z))>0 (make sure it is a matrix)
+    # check nrow(z)==length(y)
+    # check z is a matrix, and that different values of z work
+    
+    if(!is.null(z)){
+      if(is.vector(z)){
+        if(length(z)!=nrow(x)){stop("Error: length(z) must be equal to nrow(x).")}
+      }else if(!is.null(nrow(z)) & !is.null(ncol(z))){
+        if(nrow(z)!=length(y)){stop("Error: nrow(z) must be equal to length(y).")}
+      }else{stop("Error: z must either be a vector or a matrix.")}
+    }
+    
   }
-  
-}
--- R/linkRARE.R
+++ R/linkRARE.R
@@ -1,88 +1,88 @@
 linkRARE <-
-function(x,y,npl,z=NULL,SEED=1){
-
-  linkErrorCheck(x,y,npl,z)
-  
-library(SKAT)
-
-  set.seed(SEED)
-  
-#####################
-# format data
-######################
-
-# format X to be a matrix
-  # changes x to X
-X<-matrix(0,nrow=nrow(x),ncol=ncol(x))
-for(pp in 1:ncol(x)){X[,pp]<-x[,pp]}
-
-# format Z to be a matrix
-  # changes z to Z
-if(!is.null(z)){
-  if(is.vector(z)){
-    Z <- matrix(0,nrow=length(z),ncol=1)
-    for(pp in 1:length(z)){Z[pp,]<-z[pp]}
-  }
-  else{
-    Z <- matrix(0,nrow=nrow(z),ncol=ncol(z))
-    for(pp in 1:ncol(z)){Z[,pp]<-z[,pp]}
-  }
-}else{
-  Z <- NULL
-}
-
-# subjects with positive npl scores
-indZ<-c(1:length(npl))
-indZp<-indZ[(npl>0|npl==0)|is.na(npl)]
-
-# length(z)==0 when z is null
-if(length(Z)==0){
-dataP<-cbind(X,y)
-dataP<-dataP[indZp,]
-yN<-dataP[,ncol(dataP)]
-Xn<-dataP[,1:(ncol(dataP)-1)]	
-	}else{
-dataP<-cbind(X,y,Z)
-dataP<-dataP[indZp,]
-yN<-dataP[,(ncol(dataP)-ncol(Z))]
-zN<-dataP[,(ncol(dataP)-ncol(Z)+1):(ncol(dataP))]
-Xn<-dataP[,1:(ncol(dataP)-ncol(Z)-1)]
-}
-
-#########################
-## SKAT with all subjects
-#########################
-if(length(Z)==0){obj1<-SKAT_Null_Model(y ~ 1, out_type="D")
-	}else{obj1<-SKAT_Null_Model(y ~ Z, out_type="D")}
-
-modelA<-SKAT(X, obj1)
-skatAp<-modelA$p.value
-skatAn<-modelA$param$n.marker.test
-	
-modelAo<-SKAT(X, obj1,method="SKATO")
-skatApo<-modelA$p.value
-skatAno<-modelA$param$n.marker.test
-
-#########################
-## SKAT excluding subjects 
-## with NPL score <0
-#########################
-if(length(Z)==0){obj2<-SKAT_Null_Model(yN ~ 1, out_type="D")
-	}else{obj2<-SKAT_Null_Model(yN ~ zN, out_type="D")}
-
-modelP<-SKAT(Xn, obj2)
-skatPp<-modelP$p.value
-skatPn<-modelP$param$n.marker.test
-
-modelPo<-SKAT(Xn, obj2,method="SKATO")
-skatPpo<-modelP$p.value
-skatPno<-modelP$param$n.marker.test
-#########################
-## output
-#########################
-output<-list(skatAp,skatApo,skatAno,skatPp,skatPpo,skatPn)
-names(output)<-c("p-value from SKAT with all subjects","p-value from SKAT-O with all subjects",
-		 "number of rare variants with all subjects","p-value from SKAT excluding subjects with NPL<0",
-		 "p-value from SKAT-O excluding subjects with NPL<0","number of rare variants excluding subjects with NPL<0")
-
-output}
+  function(x,y,npl,z=NULL,SEED=1){
+    
+    linkErrorCheck(x,y,npl,z)
+    
+    library(SKAT)
+    
+    set.seed(SEED)
+    
+    #####################
+    # format data
+    ######################
+    
+    # format X to be a matrix
+    # changes x to X
+    X<-matrix(0,nrow=nrow(x),ncol=ncol(x))
+    for(pp in 1:ncol(x)){X[,pp]<-x[,pp]}
+    
+    # format Z to be a matrix
+    # changes z to Z
+    if(!is.null(z)){
+      if(is.vector(z)){
+        Z <- matrix(0,nrow=length(z),ncol=1)
+        for(pp in 1:length(z)){Z[pp,]<-z[pp]}
+      }
+      else{
+        Z <- matrix(0,nrow=nrow(z),ncol=ncol(z))
+        for(pp in 1:ncol(z)){Z[,pp]<-z[,pp]}
+      }
+    }else{
+      Z <- NULL
+    }
+    
+    # subjects with positive npl scores
+    indZ<-c(1:length(npl))
+    indZp<-indZ[(npl>0|npl==0)|is.na(npl)]
+    
+    # length(z)==0 when z is null
+    if(length(Z)==0){
+      dataP<-cbind(X,y)
+      dataP<-dataP[indZp,]
+      yN<-dataP[,ncol(dataP)]
+      Xn<-dataP[,1:(ncol(dataP)-1)]	
+    }else{
+      dataP<-cbind(X,y,Z)
+      dataP<-dataP[indZp,]
+      yN<-dataP[,(ncol(dataP)-ncol(Z))]
+      zN<-dataP[,(ncol(dataP)-ncol(Z)+1):(ncol(dataP))]
+      Xn<-dataP[,1:(ncol(dataP)-ncol(Z)-1)]
+    }
+    
+    #########################
+    ## SKAT with all subjects
+    #########################
+    if(length(Z)==0){obj1<-SKAT_Null_Model(y ~ 1, out_type="D")
+    }else{obj1<-SKAT_Null_Model(y ~ Z, out_type="D")}
+    
+    modelA<-SKAT(X, obj1)
+    skatAp<-modelA$p.value
+    skatAn<-modelA$param$n.marker.test
+    
+    modelAo<-SKAT(X, obj1,method="SKATO")
+    skatApo<-modelAo$p.value
+    skatAno<-modelAo$param$n.marker.test
+    
+    #########################
+    ## SKAT excluding subjects 
+    ## with NPL score <0
+    #########################
+    if(length(Z)==0){obj2<-SKAT_Null_Model(yN ~ 1, out_type="D")
+    }else{obj2<-SKAT_Null_Model(yN ~ zN, out_type="D")}
+    
+    modelP<-SKAT(Xn, obj2)
+    skatPp<-modelP$p.value
+    skatPn<-modelP$param$n.marker.test
+    
+    modelPo<-SKAT(Xn, obj2,method="SKATO")
+    skatPpo<-modelPo$p.value
+    skatPno<-modelPo$param$n.marker.test
+    #########################
+    ## output
+    #########################
+    output<-list(skatAp,skatApo,skatAn,skatPp,skatPpo,skatPn)
+    names(output)<-c("p-value from SKAT with all subjects","p-value from SKAT-O with all subjects",
+                     "number of rare variants with all subjects","p-value from SKAT excluding subjects with NPL<0",
+                     "p-value from SKAT-O excluding subjects with NPL<0","number of rare variants excluding subjects with NPL<0")
+    
+    output}
