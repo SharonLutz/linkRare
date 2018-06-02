e0eba28190aad9062caf421a8d528708dd1562a6
Sharon M. Lutz <SharonLutz@users.noreply.github.com>
2018-06-01 21:44:23 -0400

    Update linkRARE.R

--- R/linkRARE.R
+++ R/linkRARE.R
@@ -82,4 +82,7 @@ linkRARE <-
     #########################
     output<-list(skatAp,skatApo,skatAn,skatPp,skatPpo,skatPn)
  
+    names(output)<-c("p-value from SKAT with all subjects","p-value from SKAT-O with all subjects",
+                     "number of rare variants with all subjects","p-value from SKAT excluding subjects with NPL<0","p-value from SKAT-O excluding subjects with NPL<0","number of rare variants excluding subjects with NPL<0")
+    
     output}
